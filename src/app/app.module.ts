d3c007704bea45a9cd2aea86c8faa4d35fb8a9b0
Bob-development <proinvoke228@gmail.com>
2024-01-18 16:55:28 +0200

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './form/form.component';
+import { ProductComponent } from './product/product.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormComponent,
+    ProductComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
